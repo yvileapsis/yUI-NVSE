eab989735294737e0376acb51d0919da3d4e9f60
yvileapsis <yvileapsis@gmail.com>
2022-10-15 19:57:37 +0300

    Update settings.h

--- yGTM/settings.h
+++ yGTM/settings.h
@@ -3,9 +3,6 @@
 
 inline int g_logLevel					= 1;
 
-inline int g_yTM						= 1;
-inline int g_yTM_Mode					= 1;
-inline int g_yTM_MinMax					= 1;
 
 inline int g_FixExplosionPushForce		= 1;
 inline int g_AlterSpread				= 0;
@@ -17,3 +14,10 @@ inline int g_CorrectWeaponEffects		= 0;
 inline int g_ArmedUnarmed				= 1;
 
 void handleINIOptions();
+
+g_FixExplosionPushForce = ini.GetOrCreate("General", "bFixExplosionPushForce", 1, "; fix force of the explosions to scale both with distance (like explosion damage) and with actual force of the explosion's baseform.");
+g_AlterSpread = ini.GetOrCreate("General", "iAlterSpread", 1, "; restore gamesettings controlling the spread value on weapon forms.");
+g_ArmedUnarmed = ini.GetOrCreate("General", "bArmedUnarmed", 1, "; Allow for Melee and Unarmed weapons to use ammo and shoot projectiles");
+g_CorrectAmmoEffects = ini.GetOrCreate("Projectile Bugfixes", "bCorrectAmmoEffects", 0, "; EXPERIMENTAL DO NOT TOUCH");
+g_CorrectMeltdownEffects = ini.GetOrCreate("Projectile Bugfixes", "bCorrectMeltdownEffects", 0, "; EXPERIMENTAL DO NOT TOUCH");
+g_CorrectWeaponEffects = ini.GetOrCreate("Projectile Bugfixes", "bCorrectWeaponEffects", 0, "; EXPERIMENTAL DO NOT TOUCH");
