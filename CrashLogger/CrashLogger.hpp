bcaf3f29151ed34cacfc27f0a902167400f5a6db
yvileapsis <yvileapsis@gmail.com>
2024-05-23 14:36:59 +0300

    Split Labels into header and LabelsNVSE

--- CrashLogger/CrashLogger.hpp
+++ CrashLogger/CrashLogger.hpp
@@ -30,11 +30,6 @@ namespace CrashLogger::PDB
 	inline std::string GetClassNameFromRTTIorPDB(void* object);
 }
 
-namespace CrashLogger::Labels
-{
-	inline bool GetStringForLabel(void** object, std::string& buffer);
-}
-
 namespace CrashLogger
 {
 	template<typename T>
@@ -127,4 +122,171 @@ namespace CrashLogger
 			return false;
 		}
 	};
+}
+
+namespace CrashLogger::Labels
+{
+	inline std::ostream& AsUInt32(std::ostream& buffer, void* ptr) { buffer << std::format("{:#08X}", **static_cast<UInt32**>(ptr)); return buffer; }
+
+	template<typename T> std::ostream& As(std::ostream& os, void* ptr)
+	try {
+		if (auto sanitized = Dereference<T>((UInt32)ptr))
+			os << *sanitized;
+		return os;
+	}
+	catch (...) {
+		if (os.rdbuf()->in_avail()) os << ", ";
+		os << "failed to format";
+		return os;
+	}
+
+
+	class Label
+	{
+	public:
+		typedef std::ostream& (*FormattingHandler)(std::ostream& os, void* ptr);
+
+		static inline std::vector<std::unique_ptr<Label>> labels;
+		static inline FormattingHandler lastHandler = nullptr;
+
+		UInt32 address;
+		UInt32 size;
+		FormattingHandler function = lastHandler;
+		std::string name;
+
+		Label() : address(0), size(0), function(nullptr), name("") {}
+		virtual ~Label() {};
+		
+		Label(UInt32 aAddress, FormattingHandler aFunction = lastHandler, std::string aName = "", UInt32 aSize = 4)
+			: Label()
+		{
+			address = aAddress;
+			size = aSize;
+			function = aFunction;
+			name = std::move(aName);
+
+			lastHandler = aFunction;
+		};
+		
+		bool Satisfies(void* ptr) const
+		try {
+			return *static_cast<UInt32*>(ptr) >= address && *static_cast<UInt32*>(ptr) <= address + size; 
+		}
+		catch (...)
+		{
+			return false;
+		}
+
+		static std::string GetTypeName(void* ptr)
+		{
+			return PDB::GetClassNameFromRTTIorPDB(ptr);
+		}
+		
+		virtual std::string GetLabelName() const { return "None"; }
+
+		virtual std::string GetName(void* object) const { return name; }
+		
+		virtual std::string GetDescription(void* object) const
+		{
+			std::stringstream str;
+            if (function) function(str, object);
+            return str.str();
+		}
+		
+		virtual void Get(void* object, std::string& buffer) const
+		{
+			buffer += std::format("0x{:08X}", *(UInt32*)object);
+
+			buffer += " ==> ";
+			
+			buffer += GetLabelName();
+
+			buffer += ": ";
+			
+			buffer += GetName(object);
+
+			buffer += ": ";
+			
+			buffer += GetDescription(object);
+		}
+	};
+
+	class LabelClass : public Label
+	{
+	public:
+		using Label::Label;
+		
+		std::string GetLabelName() const override { return "Class"; }
+
+		std::string GetName(void* object) const override { return name.empty() ? GetTypeName(object) : name; }
+	};
+
+	class LabelGlobal : public Label
+	{
+	public:
+		using Label::Label;
+		
+		std::string GetLabelName() const override { return "Global"; }
+	};
+
+	class LabelEmpty : public Label
+	{
+	public:
+		using Label::Label;
+		
+		void Get(void* object, std::string& buffer) const override
+		{
+			return;
+		}
+	};
+	
+	template <class LabelType = LabelClass, class... _Types> void Push(_Types... args) {
+		Label::labels.push_back(std::make_unique<LabelType>(std::forward<_Types>(args)...));
+	}
+
+	void FillNVSELabels();
+	
+	inline void FillFOSELabels()
+	{
+
+	}
+
+	inline void FillOBSELabels()
+	{
+
+	}
+
+	inline void FillLabels()
+	{
+		if (g_currentGame == kFalloutNewVegas)
+			FillNVSELabels();
+		else if (g_currentGame == kFallout3) 	// thanks Stewie
+			FillFOSELabels();
+		else if (g_currentGame == kOblivion)
+			FillOBSELabels();
+	}
+
+	inline bool GetStringForLabel(void** object, std::string& buffer)
+	try 
+	{
+		static bool fillLables = false;
+
+		if (!fillLables)
+		{
+			FillLabels();
+			fillLables = true;
+		}
+
+		for (const auto& iter : Label::labels) if (iter->Satisfies(object)) 
+		{ 
+			iter->Get(object, buffer); 
+			return true; 
+		}
+		return false;
+	}
+	catch (...) { 
+		if (!buffer.empty()) buffer += ", ";
+		buffer += "failed to get string for label";
+		return true;
+	}
 }
