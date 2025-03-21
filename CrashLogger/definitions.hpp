b6a07a33795afdd7047e07140cc281646b349af9
Wall SoGB <11296448+WallSoGB@users.noreply.github.com>
2025-03-21 12:34:25 +0100

    Bump version to 5.14

--- CrashLogger/definitions.hpp
+++ CrashLogger/definitions.hpp
@@ -1,5 +1,5 @@
-#define CrashLogger_VERSION 5.13
-#define CrashLogger_VERSION_STR "5.13"
+#define CrashLogger_VERSION 5.14
+#define CrashLogger_VERSION_STR "5.14"
 #define CrashLogger_STR "CrashLogger"
 #define CrashLogger_LOG "CrashLogger.log"
 #define CrashLogger_FLD "Crash Logs"
